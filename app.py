5896e79a8e8211014973a274342e28b323bf2283
loka27e-cmd <eeshanloka@gmail.com>
2026-05-25 09:39:32 -0400

    Help function implemented

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ import sys
 
 equation = "".join(sys.argv[1:])
 
+# help navigate
+if equation == "help":
+    print("Functions:\n1. Add (1+1)\n2. Subtract (2-1)\n3. Multiply (4*3)\n4. Divide (4/2)\nEquations evaluated on a left to right basis")
+    sys.exit()
+
 # Makes it easier to read
 equation = equation.replace(" ","")
 if str(equation[0]).isnumeric() == False:
-    equation = "0"+equation
+    equation = "0"+equation # ensures adding a symbol at the front doesn't mess it up like a negative number
 
 operation_list = []
 answer = 0
